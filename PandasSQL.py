2f2127d6df2e4b0c3fef0cf03a2be6d363ca51cf
andrewLatishev <83699418+andrewLatishev@users.noreply.github.com>
2023-05-13 20:48:31 +0300

    Update PandasSQL.py

--- PandasSQL.py
+++ PandasSQL.py
@@ -2,7 +2,8 @@ import psycopg2
 from psycopg2 import Error
 import pyodbc
 import pandas as pd
-# Подключение к существующей базе данных
+import sys
+# connecting to an existing database
 connection = psycopg2.connect(user = "shad112_prmetro",\
                                 password = "1234",\
                                 host = "91.190.239.132",\
@@ -10,18 +11,18 @@ connection = psycopg2.connect(user = "shad112_prmetro",\
                                 database="SHAD112_Pr_Metro")
 
 
-def connect(connection):
-    """ Connect to the PostgreSQL database server """
-    conn = None
-    try:
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(**connection)
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-        #sys.exit(1)
-    print("Connection successful")
-    return conn
+# def connect(connection):
+#     """ Connect to the PostgreSQL database server """
+#     conn = None
+#     try:
+#         # connect to the PostgreSQL server
+#         print('Connecting to the PostgreSQL database...')
+#         conn = psycopg2.connect(**connection)
+#     except (Exception, psycopg2.DatabaseError) as error:
+#         print(error)
+#         sys.exit(1)
+#     print("Connection successful")
+#     return conn
 
 
 def postgresql_to_dataframe(conn, select_query, column_names):
@@ -39,24 +40,15 @@ def postgresql_to_dataframe(conn, select_query, column_names):
     # Naturally we get a list of tupples
     tupples = cursor.fetchall()
     cursor.close()
-
     # We just need to turn it into a pandas dataframe
     df = pd.DataFrame(tupples, columns=column_names)
+    #df = pd.DataFrame(tupples, columns=['1', '2'])
     return df
 
-conn = connect(connection)
-column_names = ["id", "source"]
 # Execute the "SELECT *" query
-df = postgresql_to_dataframe(conn, "select * from goods", column_names)
-df.head(2)
-
-# finally:
-#     if connection:
-#         cursor.close()
-#         connection.close()
-#         print("Соединение с PostgreSQL закрыто")
-
-
+df = postgresql_to_dataframe(connection, "select * from validation2023", ['1', '2', '3', '4', '5', '6', '7', '8', '9'])
+#print(df)
+print(df.head(5))
 
 
 
